b88bd1416f001a1687458e1dc481f5dc87b64a9f
Bobby <me@bobbywlindsey.com>
2016-07-22 17:43:39 -0500

    efficient appending in array

--- arrayloop.py
+++ arrayloop.py
@@ -6,3 +6,7 @@ def array_loop(array):
 array = ['a','b','c','d','e']
 
 array_loop(array)
+
+# best way to loop through elements and add to new array
+fast_array = [each+'test' for each in array]
+print(fast_array)
